55ee729363d46cb7546106bee64631d9c6eabe67
조경진 <kj_cho@idealidea.co.kr>
2019-11-25 20:20:04 +0900

    랜덤넘버 6개를 1~45, 중복 검사를 해서 집어넣고 출력

--- app/src/main/java/com/tjoeun/a201911_kotlinfinaltest/LottoActivity.kt
+++ app/src/main/java/com/tjoeun/a201911_kotlinfinaltest/LottoActivity.kt
@@ -2,9 +2,13 @@ package com.tjoeun.a201911_kotlinfinaltest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_lotto.*
 
 class LottoActivity : BaseActivity() {
 
+    var thisWeekLottoNumArrayList = ArrayList<Int>()
+    var thisWeekLottoNumTextViewArrayList = ArrayList<TextView>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,9 +19,51 @@ class LottoActivity : BaseActivity() {
 
     override fun setupEvents() {
 
+        buyOneLottoBtn.setOnClickListener {
+//            숫자를 랜덤으로 6개 생성. 1~45 / 중복 안됨.
+            setThisWeekLottoNum()
+
+        }
+
+    }
+
+    fun setThisWeekLottoNum() {
+
+        for (lottoNumTxt in thisWeekLottoNumTextViewArrayList) {
+            var randomNum = 0
+            while (true) {
+                randomNum  = (Math.random() * 45 + 1).toInt()
+
+                var isDuplOk = true
+
+                for (num in thisWeekLottoNumArrayList) {
+                    if (num == randomNum) {
+//                        중복되는 숫자를 발견!
+                        isDuplOk = false
+                        break
+                    }
+                }
+
+                if (isDuplOk) {
+                    thisWeekLottoNumArrayList.add(randomNum)
+                    break
+                }
+
+            }
+
+
+            lottoNumTxt.text = randomNum.toString()
+        }
+
+
     }
 
     override fun setValues() {
-
+        thisWeekLottoNumTextViewArrayList.add(lottoNumTxt1)
+        thisWeekLottoNumTextViewArrayList.add(lottoNumTxt2)
+        thisWeekLottoNumTextViewArrayList.add(lottoNumTxt3)
+        thisWeekLottoNumTextViewArrayList.add(lottoNumTxt4)
+        thisWeekLottoNumTextViewArrayList.add(lottoNumTxt5)
+        thisWeekLottoNumTextViewArrayList.add(lottoNumTxt6)
     }
 }
