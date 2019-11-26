b16dbba40075291d0e7131dc41e432cdbc2ae1b0
조경진 <kj_cho@idealidea.co.kr>
2019-11-26 21:33:18 +0900

    권한 / 평문통신 허용

--- app/src/main/java/com/tjoeun/a201911_kotlinfinaltest/LoginActivity.kt
+++ app/src/main/java/com/tjoeun/a201911_kotlinfinaltest/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.tjoeun.a201911_kotlinfinaltest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.tjoeun.a201911_kotlinfinaltest.utils.ContextUtil
 import com.tjoeun.a201911_kotlinfinaltest.utils.ServerUtil
 import kotlinx.android.synthetic.main.activity_login.*
@@ -24,7 +25,7 @@ class LoginActivity : BaseActivity() {
 
             ServerUtil.postRequestLogin(mContext, userId, userPw, object : ServerUtil.JsonResponseHandler {
                 override fun onResponse(json: JSONObject) {
-
+                    Log.d("서버응답", json.toString())
                 }
 
             })
