ddda0e0bf78dca03213391617abd5352fff86721
조경진 <kj_cho@idealidea.co.kr>
2019-11-26 19:32:29 +0900

    로그인 액티비티 시작

---
+++ app/src/main/java/com/tjoeun/a201911_kotlinfinaltest/LoginActivity.kt
@@ -0,0 +1,23 @@
+package com.tjoeun.a201911_kotlinfinaltest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
