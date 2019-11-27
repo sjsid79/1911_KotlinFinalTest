6ae78dde7cba72cb2ec7f72791ecf1e5da28168f
조경진 <kj_cho@idealidea.co.kr>
2019-11-27 22:19:08 +0900

    카테고리 아이콘 작업  - 첫 글자만 보여주기  - 백그라운드 색 변경

--- app/src/main/java/com/tjoeun/a201911_kotlinfinaltest/adapters/BlackListAdapter.kt
+++ app/src/main/java/com/tjoeun/a201911_kotlinfinaltest/adapters/BlackListAdapter.kt
@@ -1,6 +1,8 @@
 package com.tjoeun.a201911_kotlinfinaltest.adapters
 
 import android.content.Context
+import android.graphics.Color
+import android.graphics.PorterDuff
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -36,7 +38,10 @@ class BlackListAdapter(context:Context, res:Int, list:ArrayList<BlackList>)
         val titleTxt = row.findViewById<TextView>(R.id.titleTxt)
         val contentTxt = row.findViewById<TextView>(R.id.contentTxt)
 
-        categoryTxt.text = data.writer.category.title
+        categoryTxt.text = data.writer.category.title.first().toString()
+//        categoryTxt.text = data.writer.category.title.substring(0,1)
+//        XML로 만든 백그라운드의 컬러를 바꿔주는 코드
+        categoryTxt.background.setColorFilter(Color.parseColor(data.writer.category.color), PorterDuff.Mode.SRC_IN)
         writerNameTxt.text = data.writer.name
         titleTxt.text = data.title
         contentTxt.text = data.content
